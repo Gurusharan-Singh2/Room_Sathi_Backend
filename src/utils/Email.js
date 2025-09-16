54e9877016cb4191116d988046d1981a0f63f38d
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-09-16 10:19:56 +0530

    New update 1.4

--- src/utils/Email.js
+++ src/utils/Email.js
@@ -1,33 +1,32 @@
-import '../config/env.js'
-import nodemailer from 'nodemailer'
+import '../config/env.js';
+import nodemailer from 'nodemailer';
 
 export const sendEmail = async ({ email, subject, message }) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: process.env.SMTP_SERVICE,
-      secure: true,
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // use STARTTLS
       auth: {
         user: process.env.SMTP_MAIL,
-        pass: process.env.SMTP_PASSWORD,
+        pass: process.env.SMTP_PASSWORD, // App Password if 2FA enabled
       },
     });
 
     const mailOptions = {
-      from: process.env.SMTP_MAIL,
+      from: `"My App" <${process.env.SMTP_MAIL}>`,
       to: email,
-      subject: subject,
-      html:message
+      subject,
+      html: message,
     };
 
     const info = await transporter.sendMail(mailOptions);
     console.log("Sending email to:", email);
-    console.log('Email sent:', info.response);
-   
-
+    console.log("Email sent:", info.response);
 
     return true;
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error("Error sending email:", error);
     return false;
   }
 };
